8ed89368255cb99421ae0464d1090fb09ed4603f
Ali Rooholamini <ali.rooholamini.dev@gmail.com>
2021-11-04 16:12:53 +0330

    finished scientific Calculator v1.0

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-
-
+"strict mode";
 function AddToCalculator(value){
     document.getElementById("display").value += value;
 }
 function ResetFunc(){
     document.getElementById("display").value = "";
+}
+function Initialize(){
+    document.getElementById("display").value = (document.getElementById("display").value).replaceAll("^" , "**");
+    document.getElementById("display").value = (document.getElementById("display").value).replaceAll("÷" , "/");
+    document.getElementById("display").value = (document.getElementById("display").value).replaceAll("×" , "*");
+    document.getElementById("display").value = eval(document.getElementById("display").value);
+}
+function MathFunc(MathType){
+    document.getElementById("display").value = Math[MathType](document.getElementById("display").value);
 }
