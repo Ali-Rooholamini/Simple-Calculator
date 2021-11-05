e9c17f4adcd7a793baa61cf4dd77b1360da9436a
Ali Rooholamini <ali.rooholamini.dev@gmail.com>
2021-11-05 14:31:18 +0330

    hotfix v1.1 - fix divided by zero and show undefined when click on equal

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 "strict mode";
 function AddToCalculator(value){
+    if(document.getElementById("display").value === "cannot divide by zero"){
+        document.getElementById("display").value = "";
+    }
     document.getElementById("display").value += value;
 }
 function ResetFunc(){
     document.getElementById("display").value = "";
 }
 function Initialize(){
-    document.getElementById("display").value = (document.getElementById("display").value).replaceAll("^" , "**");
-    document.getElementById("display").value = (document.getElementById("display").value).replaceAll("÷" , "/");
-    document.getElementById("display").value = (document.getElementById("display").value).replaceAll("×" , "*");
-    document.getElementById("display").value = eval(document.getElementById("display").value);
+    if(document.getElementById("display").value == ""){
+        document.getElementById("display").value = "";
+    }
+    else{
+        document.getElementById("display").value = (document.getElementById("display").value).replaceAll("^" , "**");
+        document.getElementById("display").value = (document.getElementById("display").value).replaceAll("÷" , "/");
+        document.getElementById("display").value = (document.getElementById("display").value).replaceAll("×" , "*");
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+        if(document.getElementById("display").value === "Infinity"){
+            document.getElementById("display").value = "cannot divide by zero";
+        }
+    }
 }
 function MathFunc(MathType){
     document.getElementById("display").value = Math[MathType](document.getElementById("display").value);
